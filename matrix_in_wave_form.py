8cf3ee1530f1308afeb8c2e8b52de92bb5417ed4
ashu7mishra <ashu7mishra@gmail.com>
2024-07-29 20:50:19 +0530

    cont...

--- matrix_in_wave_form.py
+++ matrix_in_wave_form.py
@@ -1,34 +1,34 @@
-# import copy
-# def main():
-#     # YOUR CODE GOES HERE
-#     # Please take input and print output to standard input/output (stdin/stdout)
-#     # E.g. 'input()/raw_input()' for input & 'print' for output
-#     n = int(input())
-#     arr = [[0 for i in range(n)] for j in range(n)]
-#     for i in range(n):
-#         x = input().split()
-#         for j in range(n):
-#             arr[i][j] = copy.copy(x[j])
+import copy
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    n = int(input())
+    arr = [[0 for i in range(n)] for j in range(n)]
+    for i in range(n):
+        x = input().split()
+        for j in range(n):
+            arr[i][j] = copy.copy(x[j])
 
-#     j = 0
-#     i = 0
-#     while i < n:
-#         if i == n:
-#             break
-#         while j<n:
-#             print(arr[j][i], end=' ')
-#             j += 1
-#         i += 1
-#         if i == n:
-#             break
-#         while j > 0:
-#             j -= 1
-#             print(arr[j][i], end=' ')
-#         i += 1
-#     return 0
+    j = 0
+    i = 0
+    while i < n:
+        if i == n:
+            break
+        while j<n:
+            print(arr[j][i], end=' ')
+            j += 1
+        i += 1
+        if i == n:
+            break
+        while j > 0:
+            j -= 1
+            print(arr[j][i], end=' ')
+        i += 1
+    return 0
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
 
 def main():
     # DO NOT CHANGE THE CODE BELOW.
